924d2d0ba6acdef71a65ae8bde9f876bd55594f1
Octowl <alsaff1987@gmail.com>
2018-11-06 02:31:19 +0300

    Add missing submit handlers

--- src/Login.js
+++ src/Login.js
@@ -10,16 +10,22 @@ class Login extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     const { username, password } = this.state;
     return (
-      <form className="form-inline">
+      <form onSubmit={this.handleSubmit} className="form-inline">
         <input
           className="form-control mr-sm-2"
           type="text"
--- src/Signup.js
+++ src/Signup.js
@@ -11,10 +11,16 @@ class Signup extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
   }
 
   render() {
@@ -23,7 +29,7 @@ class Signup extends Component {
       <div className="col-6 mx-auto">
         <div className="card my-5">
           <div className="card-body">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className="form-group">
                 <label htmlFor="username">Username</label>
                 <input
