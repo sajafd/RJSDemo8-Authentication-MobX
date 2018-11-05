4f9234c45d9ddcfd5ddb04f7a8d709589193b3d9
Octowl <alsaff1987@gmail.com>
2018-11-06 02:12:53 +0300

    convert to class based component

--- src/Login.js
+++ src/Login.js
@@ -1,27 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const Login = props => {
-  return (
-    <form className="form-inline">
-      <input
-        className="form-control mr-sm-2"
-        type="text"
-        placeholder="Username"
-      />
-      <input
-        className="form-control mr-sm-2"
-        type="password"
-        placeholder="Password"
-      />
-      <button className="btn btn-primary my-2 my-sm-0 mr-2" type="submit">
-        Login
-      </button>
-      <Link to="/signup" className="btn btn-success my-2 my-sm-0">
-        Signup
-      </Link>
-    </form>
-  );
-};
+class Login extends Component {
+  render() {
+    return (
+      <form className="form-inline">
+        <input
+          className="form-control mr-sm-2"
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          className="form-control mr-sm-2"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="btn btn-primary my-2 my-sm-0 mr-2" type="submit">
+          Login
+        </button>
+        <Link to="/signup" className="btn btn-success my-2 my-sm-0">
+          Signup
+        </Link>
+      </form>
+    );
+  }
+}
 
 export default Login;
